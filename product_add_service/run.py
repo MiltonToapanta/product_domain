889fb5f0de7605aac187955b82b7e8c5ebfc6fe5
MiltonToapanta <matoapantav@uce.edu.ec>
2025-06-24 17:10:05 -0500

    build: :white_check_mark: Create a new script to run project and manipulate the port

---
+++ product_add_service/run.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from product_add_service import create_app, db
+import os
+
+app = create_app()
+port = int(os.getenv("PORT", 5000))
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port, debug=True)
