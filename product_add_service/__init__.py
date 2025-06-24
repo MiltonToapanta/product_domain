8ee1a6e063a2c8ebd16ed72997ef3b2b038fb15f
MiltonToapanta <matoapantav@uce.edu.ec>
2025-06-24 17:09:10 -0500

    perf: :white_check_mark: Update a init to use new strategy of table of database

--- product_add_service/__init__.py
+++ product_add_service/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, upgrade
 from flask_marshmallow import Marshmallow
 from .config import Config
 
 db = SQLAlchemy()
-migrate = Migrate()
 ma = Marshmallow()
 
 def create_app():
@@ -13,11 +11,9 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app, db)
     ma.init_app(app)
 
     from .routes.product_routes import product_bp
     app.register_blueprint(product_bp, url_prefix="/api/products")
 
     return app
-
