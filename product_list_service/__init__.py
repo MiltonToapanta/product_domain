aa3000c4bf08b0beca672c4e48f417aaf2f4eb21
MiltonToapanta <matoapantav@uce.edu.ec>
2025-06-24 19:39:41 -0500

    feat: :zap: Creating lines of code to use JWT Token

--- product_list_service/__init__.py
+++ product_list_service/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
+#import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+#Descomentar para usar validación JWT Token
+# from flask_jwt_extended import JWTManager
 from config import Config
 
 db = SQLAlchemy()
 ma = Marshmallow()
+#jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     CORS(app, resources={r"/api/*": {"origins": "*"}})
+    #app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY") 
+    #jwt.init_app(app)
     db.init_app(app)
     ma.init_app(app)
 
--- product_list_service/routes/product_routes.py
+++ product_list_service/routes/product_routes.py
@@ -2,10 +2,15 @@ from flask import Blueprint, jsonify
 from __init__ import db
 from models.product import Product
 from schemas.product_schema import products_schema
+#Descomentar para usar validación JWT Token
+#from flask_jwt_extended import jwt_required, get_jwt_identity
 
 product_bp = Blueprint("product_bp", __name__)
 
 @product_bp.route("/all", methods=["GET"])
+#@jwt_required()
 def listar_productos():
     productos = Product.query.all()
     return products_schema.jsonify(productos), 200
+
+
